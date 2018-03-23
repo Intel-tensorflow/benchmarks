9d0a90e30817b9fa4940a33d0e3b1916fb92fb52
Xiaoqiang Zheng <zhengxq@google.com>
2018-03-22 23:58:31 -0700

    Fixing the per-gpu batch size.
    PiperOrigin-RevId: 190181787

--- scripts/tf_cnn_benchmarks/benchmark_cnn.py
+++ scripts/tf_cnn_benchmarks/benchmark_cnn.py
@@ -2127,7 +2127,7 @@ class BenchmarkCNN(object):
         images = tf.reshape(
             images,
             shape=[
-                self.batch_size, image_size, image_size,
+                self.batch_size // self.num_gpus, image_size, image_size,
                 self.dataset.depth
             ])
     else:
